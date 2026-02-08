e711e5b50fb0939a5af74be4f4b41937bbeb5dbc
JoJo <j.mueller.richmann@icloud.com>
2026-02-08 16:19:29 +0100

    keine ahnung was ich gemacht habe aber start ig :)

--- Assets/Scripts/RigidBody2.cs
+++ Assets/Scripts/RigidBody2.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
 
-public class RigidBody2 : MonoBehaviour
-{
+public class RigidBody2 : MonoBehaviour {
+    
+    private Vector3 velocity = Vector3.zero;
+    
+    public int masse;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,6 @@ public class RigidBody2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += velocity * Time.deltaTime;
     }
 }
