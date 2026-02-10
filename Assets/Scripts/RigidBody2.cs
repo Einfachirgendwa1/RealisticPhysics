739df278ac741578e5e1918a295ed2a0d8eace3b
JoJo <j.mueller.richmann@icloud.com>
2026-02-10 12:04:31 +0100

    boden check + gravity implementiert + test platform mit boden

--- Assets/Scripts/RigidBody2.cs
+++ Assets/Scripts/RigidBody2.cs
@@ -1,20 +1,39 @@
 using UnityEngine;
 
-public class RigidBody2 : MonoBehaviour {
+public class RigidBody2 : MonoBehaviour
+{
+    Vector3 velocity;
+    BoxCollider col;
     
-    private Vector3 velocity = new Vector3(0,0,0);
-    
-    public int masse;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public  float gravity;
+    public float skin;
+
     void Start()
     {
-        
+        col = GetComponent<BoxCollider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position += velocity * Time.deltaTime;    
+        float castDistance = Mathf.Max(skin * 2f, Mathf.Abs(velocity.y) * Time.fixedDeltaTime);
+        Vector3 origin = transform.position + col.center - Vector3.up * (col.size.y / 2f - skin);
+        
+        //raycast downwards to check for ground
+        RaycastHit hit;
+        if (Physics.Raycast(origin, Vector3.down, out hit, castDistance)){
+            velocity.y = 0f;
+            transform.position = new Vector3(
+                transform.position.x,
+                hit.point.y + col.size.y / 2f,
+                transform.position.z
+            );
+        }
+        else
+        {
+            velocity.y -= gravity * Time.fixedDeltaTime;
+            //Debug.Log(velocity);
+        }
+
+        transform.position += velocity * Time.fixedDeltaTime;
     }
 }
