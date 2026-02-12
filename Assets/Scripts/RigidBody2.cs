08796d69c66f3327f9fafa2be26bd09513bc1ceb
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2026-02-12 14:25:30 +0100

    Refactor RigidBody2 class: streamline gravity handling and improve readability

--- Assets/Scripts/RigidBody2.cs
+++ Assets/Scripts/RigidBody2.cs
@@ -1,39 +1,27 @@
 using UnityEngine;
 
-public class RigidBody2 : MonoBehaviour
-{
-    Vector3 velocity;
-    BoxCollider col;
-    
-    public  float gravity;
+public class RigidBody2 : MonoBehaviour {
+    public float gravity;
     public float skin;
 
-    void Start()
-    {
-        col = GetComponent<BoxCollider>();
-    }
+    public BoxCollider col;
+    private Vector3 velocity;
 
-    void FixedUpdate()
-    {
+    private void FixedUpdate() {
         float castDistance = Mathf.Max(skin * 2f, Mathf.Abs(velocity.y) * Time.fixedDeltaTime);
+
         Vector3 origin = transform.position + col.center - Vector3.up * (col.size.y / 2f - skin);
-        
-        //raycast downwards to check for ground
-        RaycastHit hit;
-        if (Physics.Raycast(origin, Vector3.down, out hit, castDistance)){
+
+        if (Physics.Raycast(origin, Vector3.down, out RaycastHit hit, castDistance)) {
             velocity.y = 0f;
-            transform.position = new Vector3(
-                transform.position.x,
-                hit.point.y + col.size.y / 2f,
-                transform.position.z
-            );
-        }
-        else
-        {
+            float yPosition = hit.point.y + col.size.y / 2f;
+            transform.position = new Vector3(transform.position.x, yPosition, transform.position.z);
+        } else {
             velocity.y -= gravity * Time.fixedDeltaTime;
-            //Debug.Log(velocity);
         }
 
         transform.position += velocity * Time.fixedDeltaTime;
     }
+
+    public void AddForce(Vector3 force) => velocity += force;
 }
